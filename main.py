49f8427452fa1b455b08d77a2cbe01a74d5634c5
akorchuganoff <akorchuganoff@yandex.ru>
2021-02-18 00:20:32 +0700

    horizontal move on sprites vertical move on sprites collision with ground jump

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def draw(screen, width, height, player_position):
     pygame.draw.rect(screen, pygame.Color('Gray'), ((0, height // 4 * 3), (width, height//4)), width=0)
     #tower
     pygame.draw.rect(screen, pygame.Color((100, 100, 100)), ((width//9*4, height//8*3), (width//9, height // 8 * 3)), width=0)
-    #player
-    pygame.draw.rect(screen, pygame.Color((255, 0, 0)),
-                     ((player_position[0], player_position[1]), (20, 50)), width=0)
+    # #player
+    # pygame.draw.rect(screen, pygame.Color((255, 0, 0)),
+    #                  ((player_position[0], player_position[1]), (20, 50)), width=0)
 
 if __name__ == '__main__':
     pygame.init()
@@ -16,15 +16,51 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
 
     clock = pygame.time.Clock()
-
     running = True
 
     player_position = [width//2, height//2]
-    speed = 100
+    horizontall_speed = 200
+    vertical_speed = 500
+
+    all_sprites = pygame.sprite.Group()
+    ground_layer = pygame.sprite.Group()
+
+    class Player(pygame.sprite.Sprite):
+        def __init__(self, x, y, width, height, group):
+            super().__init__(group)
+            self.image = pygame.Surface([width, height])
+            self.image.fill((255, 0, 0))
+            self.rect = pygame.Rect(x, y, width, height)
+            self.vx = 0
+            self.vy = 0
+
+        def update(self):
+            if pygame.sprite.spritecollideany(self, ground_layer):
+                if self.vy <= 0:
+                    self.rect = self.rect.move(self.vx, self.vy)
+                else:
+                    self.rect = self.rect.move(self.vx, 0)
+            else:
+                self.rect = self.rect.move(self.vx, self.vy)
+
+    class Ground(pygame.sprite.Sprite):
+        def __init__(self, width, height, *group):
+            super().__init__(*group)
+            self.image = pygame.Surface([width, 5])
+            self.image.fill((255, 255, 255))
+            self.rect = pygame.Rect(0, height//5*4, width, 5)
+
+
+        def update(self):
+            pass
+
+    player = Player(player_position[0], player_position[1], 20, 50, all_sprites)
+    ground = Ground(width, height, [all_sprites, ground_layer])
 
     # movement triggers
     right_trigger = False
     left_trigger = False
+    jump_trigger = False
 
     while running:
         for event in pygame.event.get():
@@ -40,6 +76,9 @@ if __name__ == '__main__':
                     right_trigger = False
                     left_trigger = True
                 # horizontal move end
+                # vertical move
+                elif event.key == pygame.K_UP:
+                    jump_trigger = True
 
             if event.type == pygame.KEYUP:
                 # horizontal move begin
@@ -54,13 +93,30 @@ if __name__ == '__main__':
 
         # horizontal move begin
         if left_trigger:
-            player_position[0] -= speed * clock.tick(30) / 1000
+            player.vx = -1 * horizontall_speed * clock.tick(30) / 1000
         elif right_trigger:
-            player_position[0] += speed * clock.tick(30) / 1000
+            player.vx = horizontall_speed * clock.tick(30) / 1000
+        else:
+            player.vx = 0
         # horizontal move end
+
+        # vertical move begin
+        if jump_trigger:
+            vertical_speed = -300
+            player.vy = vertical_speed * clock.tick(30) / 1000
+            jump_trigger = False
+        else:
+            if vertical_speed >= 300:
+                vertical_speed = 300
+            else:
+                vertical_speed += 20
+            player.vy = vertical_speed * clock.tick(30) / 1000
+        # vertical move end
 
         clock.tick(30)
         screen.fill((0, 0, 0))
-        draw(screen, width, height, player_position)
+        all_sprites.draw(screen)
+        all_sprites.update()
+        # draw(screen, width, height)
         pygame.display.flip()
     pygame.quit()
